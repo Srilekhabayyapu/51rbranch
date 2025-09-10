c0db1d7d82df1a7d5928d0675b83444d21d1b0a6
srilekhabayyapu <srilekhabayyapu313@gmail.com>
2025-09-10 23:07:35 +0530

    project.html

---
+++ count.py
@@ -0,0 +1,10 @@
+m=3
+n=7
+count=0
+for num in range(m,n+1):
+    if num%2==0:
+        count=count+1
+print(num,end=',')
+    if num%2!=0:
+        count=count+1
+print(num,end=',')
---
+++ slicing.py
@@ -0,0 +1,172 @@
+# str="my name is python"
+# res=""
+# for chr in str:
+#     if chr!=" ":
+#         res=chr+res
+# rev=""
+# count=0
+# for chr in str:
+#     if chr!="":
+#        rev=rev+res[count]
+#        count+=1
+#     else:
+#         rev=rev+''
+# print(rev)
+# n=4
+# i=1
+# while i<=n:
+#     print('* '*n)
+    # i+=1
+# 
+# n=7
+# i=1
+# while i<=n:
+#     print('* '*n*2)
+#     i+=1
+## n=7
+# i=1
+# while i<=n:
+#     print('* '*i)
+#     i+=1
+# n=10
+# i=1
+# while i<=n:
+#    print('* '*i)
+# n=4
+# for i in range(1,n+1):
+#    if i==1 or i==n:
+#       print('* '*n)
+#    else:
+#        print('* '+"  " *(n-2)+'* ')
+#     n=4
+# for i in range(1,n+1):
+#   if i==1 or i==n:
+#       print('* '*n*2)
+#   else:
+#       print('* '+"  " *(n+2)+'*')
+# 
+# n=7 
+# m=3
+# for i in range(m,n+1):
+#     print(i)
+          
+# n=5
+# for i in range(n,-1,-1):
+#     print(i)
+# n=10
+# m=6
+# for i in range(n,m-1,-1):
+#     print(i)
+# n=4
+# m=2
+# sum=1
+# for i in range(m,n+1):
+#     sum=sum*i
+# print(sum)
+# n=6
+# c=0
+# for i in range(1,n+1):
+#     if n%i==0:
+#         c=c+i
+# print(c)
+# n=6
+# for i in range(1,n+1):
+#     if n%i==0:
+#        print(i)
+# n=10
+# m=3
+# for num in range(m,n+1):
+#     if num%2!=0:
+#        print(num)
+# for i in range(1,100):
+#     f=0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#            f=f+1
+#     if f==2:
+#         print(j)
+# c=0
+# for i in range(1,100):
+#     f=0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#            f=f+1
+#     if f==2:
+#         c=c+1
+# print(c)
+# n=153
+# temp=n
+# s=0
+# while n<0:
+#     r=r%10
+#     s=s+r**3
+#     n//=10
+# if temp==s:
+#     print('temp')
+# n=1
+# m=100
+# for i in range(n,m+1):w
+#     if i%2!=0:
+#         continue  
+#     else:
+#         print(i)
+# n=5
+# f=0
+# for i in range(1,n):
+#     if n%i==0:
+#         f=f+1
+# if f==1:
+#     print('prime')
+# else:
+#     print('not')
+# for n in range(1,500):
+#     l=len(str(n))
+#     temp=n
+#     sum=0
+#     while n>0:
+#         r=n%10
+#         res=r**l
+#         sum=sum+res
+#         n//=10
+#     if temp==sum:
+#         print(temp)
+# remove space from fiven text
+# str='he llo wor ld'
+# for chr in str:
+#     if chr!=" ":
+#      print(chr,end='')
+# Reverse a string
+# str='hello'
+# rev=''
+# for chr in str:
+#     rev=chr+rev
+# print(rev)
+# remove spaces and reverse the string
+# str='he llo world'
+# rev=''
+# for chr in str:
+#     if chr!=" ":
+#         rev=chr+rev
+# print(rev)
+# snake to camel case
+# string='my_variable_name'
+# n=string.split("_")
+# res=''
+# res+=n[0]
+# for chr in range(1,3):
+#    res=res+n[chr].capitalize()
+# print(res)
+# snake to pascal
+# string='my_variablr_name'
+# n=string.split('_')
+# res=''
+# for chr in n:
+#     res=res+chr.capitalize()
+# print(res)
+str="myVariableName"
+res=''
+for chr in str:
+    if str(chr).isupper:
+
+   
+    
